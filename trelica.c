6b192151124218660908b58864aae8df9cb1dce2
Joao Lucas Dantas <101306879+jldantass@users.noreply.github.com>
2025-04-28 03:03:39 -0300

    trelica.c

--- trelica.c
+++ trelica.c
@@ -1,5 +1,6 @@
 #include "trelica.h"
 #include <stdio.h>
+#include <math.h>
 
 // calcula o comprimento da barra usando as coord. dos nós
 // L = raiz( (x2-x1)² - (y2-y1)² )
@@ -18,36 +19,32 @@ void resolver_sistema_2x2(float A[2][2], float b[2], float x[2]) {
     x[1] = (A[0][0]*b[1] - A[1][0]*b[0]) / det; // x0 = (b00*a1 - b10*a0)/det
 }
 
-// calcular forças das barras
 void calcular_forcas_por_equilibrio(Barra barras[], No nos[]) {
     // identifica os nós
     int no_movel = 0; // inf direito (movel)
     int no1 = barras[0].no2; // sup esquerdo (fixo)
     int no2 = barras[1].no2; // inf esquerdo (fixo)
 
-    // vetores barra 0 (1)
-    float dx0 = nos[no1].x - nos[no_movel].x; // direcao x
-    float dy0 = nos[no1].y - nos[no_movel].y; // direcao y
-    float L0 = sqrt(dx0*dx0 + dy0*dy0); // comprimento da barra 1
-    float cos0 = dx0 / L0; // cos 1
-    float sin0 = dy0 / L0; // sen 1
+    // vetores barra 0
+    float dx0 = nos[no1].x - nos[no_movel].x;
+    float dy0 = nos[no1].y - nos[no_movel].y;
+    float L0 = sqrt(dx0*dx0 + dy0*dy0);
+    float cos0 = dx0 / L0;
+    float sin0 = dy0 / L0;
 
-    // vetores barra 1 (2)
-    float dx1 = nos[no2].x - nos[no_movel].x; // direcao x
-    float dy1 = nos[no2].y - nos[no_movel].y; // direcao y
-    float L1 = sqrt(dx1*dx1 + dy1*dy1); // comprimento da barra 2
-    float cos1 = dx1 / L1; // cos 2
-    float sin1 = dy1 / L1; // sen 2
+    // vetores barra 1
+    float dx1 = nos[no2].x - nos[no_movel].x;
+    float dy1 = nos[no2].y - nos[no_movel].y;
+    float L1 = sqrt(dx1*dx1 + dy1*dy1);
+    float cos1 = dx1 / L1;
+    float sin1 = dy1 / L1;
 
     // montar sistema
-    float A[2][2] = {
-        {cos0, cos1},
-        {sin0, sin1}
-    };
+    float A[2][2] = {{cos0, cos1}, {sin0, sin1}};
     float b[2] = {0, -F};
 
     // forças nas barras
-    float f[2]; // armazenar as duas forças
+    float f[2];
     resolver_sistema_2x2(A, b, f);
 
     barras[0].forca = f[0];
@@ -56,7 +53,6 @@ void calcular_forcas_por_equilibrio(Barra barras[], No nos[]) {
 
 // verifica se a tensao ta dentro do limite
 void verificar_tensao(Barra *barra) {
-    // calculo da tensao normal
     float sigma = fabs(barra->forca) / barra->area; // (SIGMA = |F|/A)
     barra->coef_seguranca_tensao = Sy / sigma; // COEF. SEG. TENSAO= (TENSAO DE ESCOAMENTO/SIGMA)
 }
@@ -65,7 +61,6 @@ void verificar_tensao(Barra *barra) {
 void verificar_flambagem(Barra *barra) {
     // se a barra estiver em compressao (força < 0)
     if (barra->forca < 0) {
-        // calculo da carga critica da flambagem
         float P_cr = (PI * PI * E * barra->I) / pow(barra->L, 2); // Pcr = (PI² * E * I) / L²
         barra->coef_seguranca_flambagem = P_cr / fabs(barra->forca); // COEF. SEG. FLAMBAGEM = Pcr / |F|
     } else {
@@ -73,11 +68,11 @@ void verificar_flambagem(Barra *barra) {
     }
 }
 
-// recalcula o momento de inércia (I) baseado na nova area, que vai ser utilizado para otimizar a treliça
+// recalcula o momento de inércia (I) baseado na nova area
 float recalcular_I(float area) {
     // considera a seção quadrada vazada proporcional: A = a_ext² - a_int²
     // assumindo que a_int = 0.75 * a_ext, como no exemplo original
-    float a_ext = sqrt(area / (1 - 0.75 * 0.75));
+    float a_ext = sqrt(area / (1 - 0.75*0.75));
     float a_int = 0.75 * a_ext;
     return (pow(a_ext, 4) - pow(a_int, 4)) / 12.0; // I = (a_ext^4 - a_int^4) / 12
 }
@@ -103,8 +98,8 @@ void otimizar_trelica(Barra barras[], int num_barras) {
             if (barras[i].coef_seguranca_tensao <= N_TENSAO ||
                 barras[i].coef_seguranca_flambagem <= N_FLAMBAGEM ||
                 barras[i].area <= area_minima) {
-                // reverte a ultima reduçao ja que passou do limites
-                barras[i].area /= 0.887;
+                // reverte a ultima reduçao  com uma certa correçao na area, ja que passou do limites
+                barras[i].area /= 0.887; 
                 barras[i].I = recalcular_I(barras[i].area);
                 verificar_tensao(&barras[i]);
                 verificar_flambagem(&barras[i]);
@@ -114,24 +109,60 @@ void otimizar_trelica(Barra barras[], int num_barras) {
     }
 }
 
-// imprimes os resultados
-void imprimir_resultados(No nos[], int num_nos, Barra barras[], int num_barras) {
-    printf("\n=== DESLOCAMENTOS NODAIS ===\n");
-    printf("| No | Desloc. X     | Desloc. Y \n");
-    for (int i = 0; i < num_nos; i++) {
-        printf("| %-2d | %-13.6f | %-13.6f\n", i + 1, nos[i].desloc_x, nos[i].desloc_y);
+// calcular os deslocamentos
+void calcular_deslocamentos(Barra barras[], No nos[], int num_barras) {
+
+    float K[2][2] = {{0}}; // matriz de rigidez global
+    float F_vet[2] = {0, -F}; // vetor de forças: somente carga vertical para baixo no nó móvel (F negativa em Y)
+
+    for (int i = 0; i < num_barras; i++) {
+        // verifica se a barra ta conectada no nó 0 (movel)
+        // se estiver, vai calcular os dados das barras, se nao estiver, nao vai calcular e vai pular pro proximo indice
+        // exemplo: nao vai calcular a barra do nó 1 ao 2
+        if (barras[i].no1 != 0 && barras[i].no2 != 0) continue;
+
+        // calcula as direçoes e valores para montar a matriz de rigidez
+        int no_fixo = (barras[i].no1 == 0) ? barras[i].no2 : barras[i].no1;
+        float dx = nos[no_fixo].x - nos[0].x; // direçao em X - diferença X entre nó móvel e nó fixo
+        float dy = nos[no_fixo].y - nos[0].y; // direçao em Y - diferença Y entre nó móvel e nó fixo
+        float L = barras[i].L; // comprimento da barra
+        float cos_theta = dx / L; // calcula cosseno
+        float sin_theta = dy / L; // calcula seno
+        float k = (E * barras[i].area) / L; // k(rigidez axial) = E(modulo de elasticidade) * A(area da barra) / L(comp. da barra)
+        
+        // atribui os valores e monta a matriz
+        // cada barra vai somando e contribuindo nos valores da matriz
+        K[0][0] += k * cos_theta * cos_theta;
+        K[0][1] += k * cos_theta * sin_theta;
+        K[1][0] += k * cos_theta * sin_theta;
+        K[1][1] += k * sin_theta * sin_theta;
     }
 
-    printf("\n=== FORCAS E COEF. DE SEGURANCA ===\n");
-    printf("| Barra | Forca     | Coef. Tensao  | Coef. Flambagem\n");
-    for (int i = 0; i < num_barras; i++) {
-        printf("| %-5d | %-9.2f | %-13.2f | ",
-               i + 1, barras[i].forca, barras[i].coef_seguranca_tensao);
+    // vetor de deslocamentos (D[0] = desloc_x, D[1] = desloc_y)
+    float D[2];
+     // resolve o sistema K * D = F_vet para encontrar deslocamentos Ux e Uy
+    resolver_sistema_2x2(K, F_vet, D);
+    // salva os deslocamentos calculados
+    nos[0].desloc_x = D[0];
+    nos[0].desloc_y = D[1];
+}
 
+// imprimes os resultados
+void imprimir_resultados(No nos[], int num_nos, Barra barras[], int num_barras) {
+    printf("\n======= DESLOCAMENTOS NODAIS =======\n");
+    printf("| No | Desloc. X     | Desloc. Y     |\n");
+    for (int i = 0; i < num_nos; i++) {
+        printf("| %-2d | %-13.6f | %-13.6f |\n", i + 1, nos[i].desloc_x, nos[i].desloc_y);
+    }
+
+    printf("\n=========== FORCAS E COEF. DE SEGURANCA ===========\n");
+    printf("| Barra | Forca (N) | Coef. Tensao | Coef. Flambagem |\n");
+    for (int i = 0; i < num_barras; i++) {
+        printf("| %-5d | %-9.2f | %-12.2f | ", i + 1, barras[i].forca, barras[i].coef_seguranca_tensao);
         if (isinf(barras[i].coef_seguranca_flambagem)) {
-            printf("%-15s\n", "N/A (tracionada)");
+            printf("%-15s |\n", "N/A (tracao)");
         } else {
-            printf("%-15.2f\n", barras[i].coef_seguranca_flambagem);
+            printf("%-15.2f |\n", barras[i].coef_seguranca_flambagem);
         }
     }
 
@@ -139,7 +170,5 @@ void imprimir_resultados(No nos[], int num_nos, Barra barras[], int num_barras) 
     for (int i = 0; i < num_barras; i++) {
         massa += 2800.0 * barras[i].area * barras[i].L;
     }
-
-    printf("\nMassa total da trelica: %.4f kg\n", massa);
-
+    printf("\nMassa total: %.4f kg\n", massa);
 }
